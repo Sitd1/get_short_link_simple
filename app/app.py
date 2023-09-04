cc1e86f12485a3b43e89b07eca7bb188a5589308
Denis Sitdikov <denis.sitd@yandex.ru>
2023-09-04 09:18:04 +0300

    ref

--- app/app.py
+++ app/app.py
@@ -60,6 +60,7 @@ async def get_original_url(request):
     return web.json_response({"original_url": url_data["original_url"], "request_count": url_data["request_count"]})
 
 
+
 # Обработчик для перехода по короткой ссылке
 async def redirect_short_url(request):
     short_url = request.match_info["short_url"]
